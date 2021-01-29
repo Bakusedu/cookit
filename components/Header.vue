21647dc32812f15057898be36bba831a5c192917
Chiedu Emmanuel <iamnedu912@gmail.com>
2021-01-29 04:01:27 +0100

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="flex justify-between items-center px-24">
+      <div>
+          <h2 class="uppercase tracking-wide">cookit</h2>
+      </div>
+      <ul>
+          <li>Menu</li>
+          <li>Foods</li>
+          <li>Recipe</li>
+          <li>Deserts</li>
+          <li>Favourites</li>
+      </ul>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <Header/>
+</template>
+
+<script>
+import '../components/Header';
+export default {
+  components: {
+    Header,
+  }
+}
+</script>
+
+<style>
+
+</style>
